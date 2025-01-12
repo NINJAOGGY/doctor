f03c1577364b6d261c5f2f85609b75649be60af8
Aditya Raj <adityarajdevanda1@gmail.com>
2025-01-12 22:16:14 +0530

    after update

--- App-frontend/src/context/AppContext.jsx
+++ App-frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext();
 const AppContextProvider = (props) => {
 
     const currency = '$';
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const backendUrl = 'https://doctor-backend-5dna.onrender.com'
 
     const [doctors, setDoctors] = useState([]);
     const [token,setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):false);
--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -7,7 +7,7 @@ export const DoctorContext = createContext();
 
 const DoctorContextProvider = (props) => {
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = 'https://doctor-backend-5dna.onrender.com'
 
     const [dToken,setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : ''); 
     const [appointments,setAppointments] = useState([]);
